7baf528e2ebd860eda7ae32112938434a2878002
YaokunLin <72329369+YaokunLin@users.noreply.github.com>
2021-05-09 12:32:00 -0600

    Add files via upload

---
+++ aLittleLightBulbProject.cpp
@@ -0,0 +1,28 @@
+// aLittleLightBulbProject.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+//using namespace std;
+#include <iostream>
+
+int main()
+{
+    //std::cout << "Hello World!\n";
+    //std::cout << "Hello World!!\n";
+    //std::cout << "Hello World!!!\n";
+
+    std::cout << "     :\n";
+    std::cout << " '.  _  .''\n";
+    std::cout << "-=  (~)  =-   \n";
+    std::cout << " .'  #  '.\n";
+
+    //std: “using namespace std” means we use the namespace named std. 
+    //“std” is an abbreviation for standard. So that means we use all the things with in “std” namespace.
+    //std::cout  use the definition of cout from std namespace.
+
+    //The "c" in cout refers to "character" and 'out' means "output", hence cout means "character output".    
+    //The cout object is used along with the insertion operator << in order to display a stream of characters.
+    
+
+    system("pause>0");
+}
+
+
